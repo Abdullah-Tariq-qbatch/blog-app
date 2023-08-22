0476df9159af1a70429b8803400b748a99f768ee
Abdullah-Tariq-qbatch <abdullah.tariq@qbatch.com>
2023-08-22 13:50:26 +0500

    Resolved errors

--- src/components/HomePage/Blogs.jsx
+++ src/components/HomePage/Blogs.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/button-has-type */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-named-as-default */
@@ -52,13 +53,30 @@ function Blogs() {
   return (
     <div className="mt-10 text-center text-2xl font-serif">
       <h1 className="text-3xl">Some Interesting Reads</h1>
-      <input
+      {/* <input
         type="text"
         value={searchTerm}
         onChange={handleSearchChange}
         placeholder="Search"
         className="float-right text-base mr-24"
-      />
+      /> */}
+
+      <div className="relative w-72 float-right -mt-12 mr-10">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+          </svg>
+        </div>
+        <input
+          type="search"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          id="default-search"
+          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Search Blogs..."
+          required
+        />
+      </div>
 
       <div className="flex justify-center mt-10">
         {BlogsData.loading || UserData.loading || CommentData.loading ? (
