5f6547e17578bf854b901739c03ce8600e00fd6f
Abdullah-Tariq-qbatch <abdullah.tariq@qbatch.com>
2023-08-29 14:54:46 +0500

    resolved eslint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,93 @@
-
-import React, { lazy, Suspense } from 'react';
+import React, { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/catalogApp/Layout";
 import LazyLoading from "./components/catalogApp/LazyLoading";
-import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-import Spinner from './components/social-media-feed/Spinner/Spinner';
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import Root from "./components/tvShowApp/Root";
-import Loader from "./components/tvShowApp/Loader";
 
-
-const Header = lazy(() => import('./components/social-media-feed/Header/Header'));
-const UsersFeed = lazy(() => import('./pages/social-media-feed/UsersFeed/UsersFeed'));
-const AddPost = lazy(() => import('./pages/social-media-feed/AddPost/AddPost'));
-const PostsFeed = lazy(() => import('./pages/social-media-feed/PostsFeed/PostsFeed'));
+const Header = lazy(() =>
+    import("./components/social-media-feed/Header/Header")
+);
+const UsersFeed = lazy(() =>
+    import("./pages/social-media-feed/UsersFeed/UsersFeed")
+);
+const AddPost = lazy(() => import("./pages/social-media-feed/AddPost/AddPost"));
+const PostsFeed = lazy(() =>
+    import("./pages/social-media-feed/PostsFeed/PostsFeed")
+);
 const AllTvShows = lazy(() =>
-  import(/* webpackChunkName: "allTvShows" */ "./pages/tvShowApp/AllTvShows")
+    import(/* webpackChunkName: "allTvShows" */ "./pages/tvShowApp/AllTvShows")
 );
 const AddTvShow = lazy(() =>
-  import(/* webpackChunkName: "addTvShow " */ "./pages/tvShowApp/AddTvShow")
+    import(/* webpackChunkName: "addTvShow " */ "./pages/tvShowApp/AddTvShow")
 );
 const TvShowDetails = lazy(() =>
-  import(/* webpackChunkName: "addTvShow " */ "./pages/tvShowApp/TvShowDetails")
+    import(
+        /* webpackChunkName: "addTvShow " */ "./pages/tvShowApp/TvShowDetails"
+    )
 );
 
 const Page404 = lazy(() =>
-  import(/* webpackChunkName: "page404 " */ "./components/tvShowApp/Page404")
+    import(/* webpackChunkName: "page404 " */ "./components/tvShowApp/Page404")
 );
 
 const ProductForm = lazy(() =>
-  import(/* webpackChunkName: "productForm" */ "./pages/catalogApp/ProductForm")
+    import(
+        /* webpackChunkName: "productForm" */ "./pages/catalogApp/ProductForm"
+    )
 );
 const AllProduct = lazy(() =>
-  import(/* webpackChunkName: "allProducts" */ "./pages/catalogApp/AllProducts")
+    import(
+        /* webpackChunkName: "allProducts" */ "./pages/catalogApp/AllProducts"
+    )
 );
 
 function App() {
-  return (
-    <BrowserRouter>
- 
-
-    
-         <LazyLoading>
-          <Routes>
-            <Route path="/tvShows" element={<Root />}>
-              <Route path="add-tv-show" element={<AddTvShow />} />
-              <Route path="all-tv-shows" element={<AllTvShows />} />
-              <Route path="tv-show-details/:id" element={<TvShowDetails />} />
-              <Route path="*" element={<Page404 />} />
-            </Route>
+    return (
+        <BrowserRouter>
+            <LazyLoading>
             <ToastContainer />
-           <Route path="/" element={<Header />}>
-            <Route path="/" element={<PostsFeed />} />
-            <Route path="/users-feed" element={<UsersFeed />} />
-            <Route
-              path="/posts-feed/user"
-              element={<PostsFeed pageLink="user" />}
-            />
-            <Route
-              path="/my-posts"
-              element={<PostsFeed pageLink="my-posts" />}
-            />
-            <Route path="/add-post" element={<AddPost />} />
-            <Route
-              path="/edit-post"
-              element={<AddPost pageLink="edit" />}
-            />
-          </Route>
-<Route path="/catalog" element={<Layout />}>
-            <Route exact path="" element={<AllProduct />} />
-            <Route path="add" element={<ProductForm />} />
-            <Route path="edit" element={<ProductForm />} />
-          </Route>
-          </Routes>
-          </LazyLoading>
-    </BrowserRouter>
-  );
+                <Routes>
+                    <Route path="/tvShows" element={<Root />}>
+                        <Route path="add-tv-show" element={<AddTvShow />} />
+                        <Route path="all-tv-shows" element={<AllTvShows />} />
+                        <Route
+                            path="tv-show-details/:id"
+                            element={<TvShowDetails />}
+                        />
+                        <Route path="*" element={<Page404 />} />
+                    </Route>
+            
+                    
+                    <Route path="/" element={<Header />}>
+                        <Route path="/" element={<PostsFeed />} />
+                        <Route path="/users-feed" element={<UsersFeed />} />
+                        <Route
+                            path="/posts-feed/user"
+                            element={<PostsFeed pageLink="user" />}
+                        />
+                        <Route
+                            path="/my-posts"
+                            element={<PostsFeed pageLink="my-posts" />}
+                        />
+                        <Route path="/add-post" element={<AddPost />} />
+                        <Route
+                            path="/edit-post"
+                            element={<AddPost pageLink="edit" />}
+                        />
+                    </Route>
+                    
+                    <Route path="/catalog" element={<Layout />}>
+                        <Route exact path="" element={<AllProduct />} />
+                        <Route path="add" element={<ProductForm />} />
+                        <Route path="edit" element={<ProductForm />} />
+                    </Route>
+                </Routes>
+            </LazyLoading>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/social-media-feed/AddPost/AddPost.js
+++ src/pages/social-media-feed/AddPost/AddPost.js
@@ -1,4 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
