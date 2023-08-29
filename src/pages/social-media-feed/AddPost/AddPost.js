409095f6c582a11be94c3771cd4e4f38425f1121
ahmad-hassan-qbatch <muhammad.ahmadhassan@qbatch.com>
2023-08-29 16:15:20 +0500

    css

--- src/pages/social-media-feed/AddPost/AddPost.js
+++ src/pages/social-media-feed/AddPost/AddPost.js
@@ -1,6 +1,4 @@
 
-/* eslint-disable react/prop-types */
-
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
