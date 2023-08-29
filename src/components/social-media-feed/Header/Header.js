f2ce2a29805ae0a7577b0c697a700ac461d7bcdc
Mac <abdullah.faisal@qbatch.com>
2023-08-29 16:09:25 +0500

    updated

--- src/components/social-media-feed/Header/Header.js
+++ src/components/social-media-feed/Header/Header.js
@@ -10,7 +10,7 @@ import { ReactComponent as MenuIcon } from "./../../../assets/social-media-feed/
 const Header = () => {
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-[#e5e7eb]">
+      <nav className="bg-gray-100 border-gray-300 dark:bg-[#e5e7eb]">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <NavLink to="/">
             <div className="flex items-center">
@@ -38,7 +38,13 @@ const Header = () => {
             className="hidden w-full md:block md:w-auto"
             id="navbar-default "
           >
-            <ul className="font-medium text-black dark:text-black flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-[#e5e7eb] md:dark:bg-[#e5e7eb] dark:border-gray-700 ">
+            <ul
+              className="font-medium
+             text-black dark:text-black
+              flex flex-col p-4 md:p-0 mt-4 
+              border border-gray-100 rounded-lg
+               bg-gray-100 md:flex-row md:space-x-8 md:mt-0 md:border-0"
+            >
               <li>
                 <NavLink
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#4E67E4] md:p-0 dark:text-black md:dark:hover:text-[#4E67E4] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
