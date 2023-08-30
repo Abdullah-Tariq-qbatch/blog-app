959e0f98935c62aaa2ef27d16075c7525dc47a72
ahmad-hassan-qbatch <muhammad.ahmadhassan@qbatch.com>
2023-08-30 18:04:10 +0500

    Pagination Bug Fixed

--- src/components/tvShowApp/Pagination.jsx
+++ src/components/tvShowApp/Pagination.jsx
@@ -121,8 +121,8 @@ const Pagination = ({ searchParam, pageParam }) => {
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">{currPage}</span> of{" "}
-            <span className="font-medium">{totalPages}</span> totalPages
+            Showing <span className="font-medium">{`${currPage}`}</span> of{" "}
+            <span className="font-medium">{`${totalPages}`}</span> totalPages
           </p>
         </div>
         <div>
