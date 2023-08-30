ed51e3e5b9c57696ebab6b2b19edc36046020119
ahmad-hassan-qbatch <muhammad.ahmadhassan@qbatch.com>
2023-08-30 15:24:55 +0500

    Product Add form colors bug fixed

--- src/components/catalogApp/Cards/Colors.jsx
+++ src/components/catalogApp/Cards/Colors.jsx
@@ -1,19 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import Button from "../Button";
 
 const Colors = ({ sizeData, colors, selectedSize }) => {
   return (
     <>
-      {colors[sizeData[selectedSize]?.name].map((color, index) => {
-        return (
-          <span
-            key={`${index}`}
-            className={`border-2 rounded-full border-transparent mr-3 p-2`}
-            style={{ backgroundColor: color.hex }}
-            onClick={() => {}}
-          ></span>
-        );
-      })}
+      {colors[sizeData[selectedSize]?.name] !== undefined ? (
+        colors[sizeData[selectedSize]?.name].map((color, index) => {
+          return (
+            <span
+              key={`${index}`}
+              className={`border-2 rounded-full border-transparent mr-3 p-2`}
+              style={{ backgroundColor: color.hex }}
+              onClick={() => {}}
+            ></span>
+          );
+        })
+      ) : (
+        <Button
+          className="text-sm border-0 border-black mr-3"
+          label="Colors Not Available"
+        />
+      )}
     </>
   );
 };
