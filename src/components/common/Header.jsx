2f9d828c8f30e13fb4f6cce65a01628da2932e9f
Abdullah-Tariq-qbatch <abdullah.tariq@qbatch.com>
2023-09-07 14:53:06 +0500

    Common Header

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import Switcher from '../blogApp/Switcher';
+import { ReactComponent as HamburgerSvg } from '../../assets/blogApp/svg/hamburger.svg';
+import { ReactComponent as CrossSvg } from '../../assets/blogApp/svg/cross.svg';
+
+import { RenderIf } from '../../utils/blogApp/commonMethods';
+
+function Header({ logo, links }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-gray-100 dark:bg-gray-900 p-5 mb-8 z-40">
+      <div className="flex items-center justify-between flex-wrap mx-10">
+        <Link to="/blog/" className="flex items-center flex-shrink-0 text-white mr-16 cursor-pointer">
+          <img src={logo} className="w-10 h-10 invert-0 dark:invert" alt="Logo" />
+        </Link>
+        <div className="block lg:hidden">
+          <button
+            onClick={handleClick}
+            className="flex items-center px-3 py-2 rounded text-black dark:text-white hover:text-black-400"
+            type="button"
+          >
+            <RenderIf
+              isTrue={isOpen}
+              fallback={(
+                <HamburgerSvg className="fill-current h-3 w-3 block" />
+            )}
+            >
+              <CrossSvg className="fill-current h-3 w-3 block" />
+            </RenderIf>
+          </button>
+        </div>
+        <div
+          className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
+            isOpen ? 'block' : 'hidden'
+          }`}
+        >
+          <div className="text-sm lg:flex-grow flex-col lg:flex-row flex items-start mb-5 lg:mb-0">
+            {links?.map((link) => (
+              <Link to={link.url} key={link.text} className="block mt-4 lg:inline-block lg:mt-0 text-gray-700 dark:text-gray-200 mr-4" onClick={handleClick}>{link.text}</Link>
+            ))}
+          </div>
+          <Switcher />
+        </div>
+      </div>
+    </nav>
+
+  );
+}
+export default Header;
--- src/layout/blogApp/MainLayout.jsx
+++ src/layout/blogApp/MainLayout.jsx
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Outlet } from "react-router-dom";
 
-import Navbar from '../../components/blogApp/Navbar';
+import Header from '../../components/common/Header'
 import Footer from '../../components/blogApp/Footer';
 import Toast from '../../components/blogApp/Toast';
+import logo from '../../assets/blogApp/image/png/logo512.png';
 
 import { fetchUsers } from '../../redux/users/actionCreator';
 import { fetchBlogs } from '../../redux/blogs/actionCreator';
@@ -19,9 +20,20 @@ export default function MainLayout() {
     dispatch(fetchUsers());
     dispatch(fetchComments());
   }, []);
+
+  const links = [
+    {
+      text: 'Blog Home',
+      url: '/blog/'
+    },
+    {
+      text: 'Write a Blog',
+      url: '/blog/create-blog'
+    },
+  ]
   return (
     <div className="mt-20 h-full w-full bg-white dark:bg-gray-800 ease-linear selection:bg-gray-800 selection:text-gray-200 dark:selection:bg-gray-200 dark:selection:text-gray-800">
-      <Navbar />
+      <Header links={links} logo={logo} />
       <Toast />
       <Outlet />
       <Footer />
