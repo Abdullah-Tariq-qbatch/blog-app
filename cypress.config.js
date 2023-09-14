c26a89c74a33dbe333f5b385f74dbf35cb23cd89
Abdullah Tariq <139118353+Abdullah-Tariq-qbatch@users.noreply.github.com>
2023-09-14 16:21:20 +0500

    Complete Blog App Testing (#73)

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
-  },
-
   e2e: {
-    baseUrl: "https://blog-app-gamma-swart.vercel.app",
-    // watchForFileChanges: false,
+    baseUrl: 'https://blog-app-gamma-swart.vercel.app',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,15 +11,6 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-Cypress.Commands.add("login", () => {
-  cy.viewport("macbook-16");
-  const email = "faiq@gmail.com";
-  const password = "1234Faiq";
-
-  cy.visit("/login");
-  cy.get('[name="email"]').type(`${email}`);
-  cy.get('[name="password"]').type(`${password}{enter}`);
-});
 //
 //
 // -- This is a child command --
@@ -32,3 +23,27 @@ Cypress.Commands.add("login", () => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", () => {
+  const email = "john@mail.com";
+  const password = "changeme";
+  cy.session([email, password], () => {
+    cy.visit("/login");
+    cy.get('[name="email"]').type(email);
+    cy.get('[name="password"]').type(`${password}{enter}`);
+    cy.get('[href="/blog"]').click();
+  }, {
+    validate() {
+      cy.visit("/");
+    },
+    cacheAcrossSpecs: true,
+  });
+});
+
+Cypress.Commands.add("goToBlog", () => {
+  cy.visit("/");
+  cy.get('[href="/blog"]').click();
+});
+
+Cypress.Commands.add("goToBlogDetailPage", () => {
+  cy.get(':nth-child(1) > .bg-gray-50 > .mt-2 > a > .mb-2').click();
+});
