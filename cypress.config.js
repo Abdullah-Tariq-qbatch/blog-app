1875bad709577f280f06ce05879b30a6c4ddc29f
Faiq Shahzad <139221615+faiq-shahzad-qbatch@users.noreply.github.com>
2023-09-14 16:49:59 +0500

    Cypress faiq (#75)
    * Test cases for user authentication system
    
    * User Authentication Module Test Cases
    
    * updated login command
    
    * deleted unnecessary folders

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require("cypress");
+// Populate process.env with values from .env file
+require("dotenv").config();
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://blog-app-gamma-swart.vercel.app',
+    baseUrl: "https://localhost:3000",
+    env: {
+      email: process.env.EMAIL,
+      password: process.env.PASSWORD,
+      googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      googleClientId: process.env.REACT_APP_GOOGLE_CLIENTID,
+      googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
+    },
+    // defaultCommandTimeout: 20000, // Set a default command timeout of 10 seconds for all commands
+    // execTimeout: 20000, // Set a global assertion timeout of 20 seconds
     setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
